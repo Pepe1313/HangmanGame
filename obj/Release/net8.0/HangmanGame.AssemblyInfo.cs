add11464d37fd0c3e3d7eb3763984f2c10e0a67a
Pepe Finster <pepefinster@gmail.com>
2024-06-24 15:55:57 +0200

    Add publish directory for GitHub Pages

--- obj/Release/net8.0/HangmanGame.AssemblyInfo.cs
+++ obj/Release/net8.0/HangmanGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangmanGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144fe130eed31d0d47692129a24e448bf24ef350")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangmanGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangmanGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
